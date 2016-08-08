588f6f742c2ec93519045f83373d5f40e1165ef7
Sven Gehring <sgehring@kochag.ch>
2016-08-08 09:06:56 +0200

    Documented debug functions

--- core/debug.lua
+++ core/debug.lua
@@ -7,10 +7,18 @@
 -- file: core/debug.lua
 -- desc: Helper function for debugging
 
+
+--- Log a given string and optional values.
+-- @param message The message to output in the log
+-- @param ... Optional values that will be concatenated to the message
 function zythum_log (message, ...)
   print('zythum> ' .. string.format(message, ...))
 end
 
+
+--- Log a serpent print block of an object.
+-- This method may be used for debugging only
+-- @param object The object that should be printed
 function zythum_debug (object)
   print(serpent.block(object))
 end
