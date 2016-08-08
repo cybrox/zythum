2368fbc50c31345bac4bc3c2d0e6365c92453ce7
Sven Gehring <sgehring@kochag.ch>
2016-08-08 08:16:29 +0200

    Added basic mod structure

---
+++ config.lua
@@ -0,0 +1,10 @@
+-- zythum sorter
+-- tidy up your factorio ui
+--
+-- written and copyrighted 2016+
+-- by sven marc 'cybrox' gehring
+--
+-- file: config.lua
+-- desc: Mod config
+
+zythum_debug = true
---
+++ data-final-fixes.lua
@@ -0,0 +1,10 @@
+-- zythum sorter
+-- tidy up your factorio ui
+--
+-- written and copyrighted 2016+
+-- by sven marc 'cybrox' gehring
+--
+-- file: data-final-fixes.lua
+-- desc: Past-load after all other mods, initialize sorting
+
+require('config')
