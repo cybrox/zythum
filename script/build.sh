44bf223c55d17775607ad59e9124bf476e6cd8a3
Sven Gehring <sgehring@kochag.ch>
2016-08-10 10:10:25 +0200

    Compile mods into one file when building

--- script/build.sh
+++ script/build.sh
@@ -10,19 +10,26 @@ if [[ -e "./info.json" ]]; then
   echo "$VERSION"
 
   mkdir dist
+  mkdir dist/mods
 
   cp -R ./assets ./dist
   cp -R ./core ./dist
   cp -R ./locale ./dist
-  cp -R ./mods ./dist
   cp -R ./prototypes ./dist
+
+  echo "-- zythum compiled mod library" > ./dist/mods/compiled.lua
+  ls ./mods | while read FILE; do
+    if [[ "$FILE" =~ \.gitkeep || "$FILE" =~ \_template ]]; then
+      continue
+    fi
+
+    cat "./mods/$FILE" | grep zythum_sort >> ./dist/mods/compiled.lua
+  done
 
   cp info.json ./dist/
   cp config.lua ./dist/
   cp data-final-fixes.lua ./dist/
   cp LICENSE.md ./dist/LICENSE
-
-  rm ./dist/mods/_template.lua
 
   cd ./dist
   zip -r "../zythum_$VERSION.zip" ./*
