b65102092736cc56bf4815c5a7fed06ce24da1a1
Sven Gehring <cbrxde@gmail.com>
2016-08-13 20:25:13 +0200

    Oopsie forgot 5dim_ores

---
+++ mods/5dim_ores.lua
@@ -0,0 +1,40 @@
+-- zythum sorter
+-- tidy up your factorio ui
+--
+-- file: mods/5dim_ores.lua
+-- name: 5dim Ores
+-- link: https://mods.factorio.com/mods/McGuten/5dim_ores
+-- author: cybrox
+-- refver: 0.13.1
+
+zythum_sort_mod('5dim_ores')
+
+-- Recipes for category zythum-logistics
+zythum_sort('logistics',     06, 01, '5d-gold-wire')
+zythum_sort('logistics',     06, 02, '5d-aluminium-wire')
+
+-- Recipes for category zythum-intermediate
+zythum_sort('intermediate',  04, 01, '5d-tin-gear-wheel')
+zythum_sort('intermediate',  04, 02, '5d-zinc-gear-wheel')
+zythum_sort('intermediate',  04, 03, '5d-aluminium-gear-wheel')
+
+zythum_sort('intermediate',  02, 01, '5d-gold-circuit')
+
+-- Recipes for category zythum-resources
+zythum_sort('resources',     06, 01, '5d-tin-dust')
+zythum_sort('resources',     06, 02, '5d-lead-dust')
+zythum_sort('resources',     06, 03, '5d-zinc-dust')
+zythum_sort('resources',     06, 04, '5d-gold-dust')
+zythum_sort('resources',     06, 05, '5d-aluminium-dust')
+
+-- Recipes for category zythum-plates
+zythum_sort('plates',        01, 01, '5d-tin-plate')
+zythum_sort('plates',        01, 01, '5d-tin-plate-2')
+zythum_sort('plates',        01, 02, '5d-lead-plate')
+zythum_sort('plates',        01, 02, '5d-lead-plate-2')
+zythum_sort('plates',        01, 03, '5d-zinc-plate')
+zythum_sort('plates',        01, 03, '5d-zinc-plate-2')
+zythum_sort('plates',        01, 04, '5d-gold-plate')
+zythum_sort('plates',        01, 04, '5d-gold-plate-2')
+zythum_sort('plates',        01, 05, '5d-aluminium-plate')
+zythum_sort('plates',        01, 05, '5d-aluminium-plate-2')
