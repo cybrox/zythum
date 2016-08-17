f9609c261b4a40f53eae6fe88a0e4a5a8e561e67
Aes Sedai <zrlovin@gmail.com>
2016-08-17 17:50:13 -0500

    Add SmartTrains mod

---
+++ mods/SmartTrains.lua
@@ -0,0 +1,13 @@
+-- zythum sorter
+-- tidy up your factorio ui
+--
+-- file: mods/SmartTrains.lua
+-- name: Smart Trains
+-- link: github.com/Choumiko/SmartTrains
+-- author: Aes Sedai
+-- refver: 0.4.7
+
+zythum_sort_mod('SmartTrains')
+
+-- Recipes for category zythum-vehicles
+zythum_sort('vehicles',      03, 02, 'smart-train-stop')
